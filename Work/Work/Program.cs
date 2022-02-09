d2b717170effaf9edfd3ee765b3abee28d3b40f5
Mike-Skvortsov <mixaylo.skv@gmail.com>
2022-02-09 15:50:40 +0200

    Homework/Lesson-3-condition-loops

--- Work/Work/Program.cs
+++ Work/Work/Program.cs
@@ -6,7 +6,65 @@ namespace Work
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+
+									//Task 2
+			bool Validity;
+			string number;
+			Console.Write("Hi, enter your first number: ");
+			number = Console.ReadLine();
+			Validity = int.TryParse(number, out int X);
+			if (Validity == false)
+			{
+				Console.WriteLine("Invalid input");
+				return;
+			}
+			Console.Write("Hi, enter your second number: ");
+			number = Console.ReadLine();
+			Validity = int.TryParse(number, out int Y);
+			if (Validity == false)
+			{
+				Console.WriteLine("Invalid input");
+				return;
+			}
+			int sum = 0;
+			if (X == Y)
+			{
+				Console.WriteLine(X);
+				return;
+			}
+			if (X < Y)
+			{
+				for (int i = X; i <= Y; i++)
+				{
+					sum += i;
+				}
+			}
+			else if(Y < X)
+			{
+				for (int i = Y; i <= X; i++)
+				{
+					sum += i;
+				}
+			}
+
+			Console.Write(sum);
+			//Task 1
+			//const int a = 10;
+			//const int b = 13;
+			//if (a == b)
+			//{
+			//	Console.Write(a);
+			//	return;
+			//}
+			//for (int i = 0; i <= b; i++)
+			//{
+			//	sum += i;
+			//	if(a==i)
+			//	{
+			//		Console.Write(sum);
+			//		return;
+			//	}
+			//}
 		}
 	}
 }
